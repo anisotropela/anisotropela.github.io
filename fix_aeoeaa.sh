17e9a8a0b7d435cbc48346d8a6a293a00a626128
Peter Laursen <pela@dark-cosmology.dk>
2020-01-14 11:30:28 +0100

    backup before fix_aeoeaa

---
+++ fix_aeoeaa.sh
@@ -0,0 +1,17 @@
+sed -i '' -e 's/&aelig;/æ/g'    *.html
+sed -i '' -e 's/&Aelig;/æ/g'    *.html
+sed -i '' -e 's/&oslash;/ø/g'   *.html
+sed -i '' -e 's/&Oslash;/ø/g'   *.html
+sed -i '' -e 's/&aring;/å/g'    *.html
+sed -i '' -e 's/&Aring;/å/g'    *.html
+sed -i '' -e 's/&eacute;/é/g'   *.html
+sed -i '' -e 's/&Eacute;/é/g'   *.html
+
+sed -i '' -e 's/&aelig;/æ/g'    */*.html
+sed -i '' -e 's/&Aelig;/æ/g'    */*.html
+sed -i '' -e 's/&oslash;/ø/g'   */*.html
+sed -i '' -e 's/&Oslash;/ø/g'   */*.html
+sed -i '' -e 's/&aring;/å/g'    */*.html
+sed -i '' -e 's/&Aring;/å/g'    */*.html
+sed -i '' -e 's/&eacute;/é/g'   */*.html
+sed -i '' -e 's/&Eacute;/é/g'   */*.html
